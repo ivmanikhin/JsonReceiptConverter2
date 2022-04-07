8eb10b157b9102702a853df4a5575c3aa5c96358
ivmanikhin <ivmanikhin@gmail.com>
2022-04-07 17:31:48 +0300

    1:35:14

--- main.py
+++ main.py
@@ -1,22 +1,37 @@
 from kivy.app import App
+# from kivy.uix.boxlayout import BoxLayout
+# from kivy.uix.button import Button
 from kivy.properties import StringProperty
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.widget import Widget
 
 
 class WidgetsExample(GridLayout):
     count = 0
-    label_text = StringProperty("0")
-    toggle_button_state = StringProperty("0")
+    slider_value = 50
+    # count_label_text = StringProperty(str(count))
+    # slider_value_text = StringProperty(str(slider_value))
+    # toggle_button_state = StringProperty("Off")
 
     def button_toggle(self, widget):
-        self.toggle_button_state = str(widget.state)
+        if widget.state == "down":
+            widget.text = "On"
+            self.ids.count_button.disabled = False
+        else:
+            widget.text = "Off"
+            self.ids.count_button.disabled = True
 
     def button_change_label_text(self):
         self.count += 1
-        self.label_text = str(self.count)
+        self.ids.counter_label.text = str(self.count)
+
+    def on_switch_active(self, widget):
+        print("Switch " + ("On" if widget.active else "Off"))
+
+    def on_slider_value(self, widget):
+        self.slider_value = int(widget.value)
+        # self.slider_value_text = str(self.slider_value)
+        # self.ids.slider_label.text = str(self.slider_value)
 
     # pass
     # def __init__(self, **kwargs):
