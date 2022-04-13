bb99bb95253e2c4827e4c2c49f75cf051dfc0083
ivmanikhin <ivmanikhin@gmail.com>
2022-04-13 12:52:55 +0300

    now it handles batches

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import json
 
 from kivy.core.clipboard import Clipboard
 from kivy.core.window import Window
+from kivy.metrics import dp
 from kivy.uix.button import Button
 # from kivy.properties import StringProperty
 
@@ -34,18 +35,18 @@ class MainWidget(ScrollView):
         try:
             if not "ticket" in raw_json[0].keys():
                 for _ in range(len(raw_json)):
-                    self.dir_buttons.append(Button(text=raw_json[_]['localDateTime'].replace('T', ' ') + " " + raw_json[_]['retailPlace'],
-                                                   size_hint=(1, None), height="40dp", split_str="True", text_size=(Window.width - 20, None),
+                    self.dir_buttons.append(Button(text=raw_json[_]['localDateTime'].replace('T', ' ') + "\n" + raw_json[_]['retailPlace'] + "\n" + '{:.2f}'.format(raw_json[_]['totalSum'] * 0.01) + " р.",
+                                                   size_hint=(1, None), height="80dp", split_str="True", text_size=(Window.width - dp(20), None),
                                                    on_press=partial(self.convert_json_to_text, raw_json[_])))
                     self.ids.main_layout.add_widget(self.dir_buttons[_])
             else:
-                print("Ticket length: " + str(len(raw_json)))
+                # print("Ticket length: " + str(len(raw_json)))
                 for _ in range(len(raw_json)):
                     json_item = raw_json[_]["ticket"]["document"]["receipt"]
-                    print(json_item)
+                    # print(json_item)
                     json_item['localDateTime'] = json_item['dateTime']
-                    self.dir_buttons.append(Button(text=json_item['localDateTime'].replace('T', ' ') + " " + json_item['retailPlace'],
-                                                   size_hint=(1, None), height="40dp", split_str="True", text_size=(Window.width - 20, None),
+                    self.dir_buttons.append(Button(text=json_item['localDateTime'].replace('T', ' ') + "\n" + json_item['retailPlace'] + "\n" + '{:.2f}'.format(json_item['totalSum'] * 0.01) + " р.",
+                                                   size_hint=(1, None), height="80dp", split_str="True", text_size=(Window.width - dp(20), None),
                                                    on_press=partial(self.convert_json_to_text, json_item)))
                     self.ids.main_layout.add_widget(self.dir_buttons[_])
         except Exception as e:
@@ -57,11 +58,11 @@ class MainWidget(ScrollView):
             with open(uri.replace('"', ''), "r", encoding="utf-8") as file:
                 json_receipt = json.load(file)
             if isinstance(json_receipt, list):
-                print(type(json_receipt))
-                print("json is list")
+                # print(type(json_receipt))
+                # print("json is list")
                 self.convert_batch_json_to_text(json_receipt)
             else:
-                print("single receipt")
+                # print("single receipt")
                 self.convert_json_to_text(json_receipt)
         except Exception as e:
             self.ids.converted_json.text = str(e)
@@ -85,7 +86,7 @@ class MainWidget(ScrollView):
                     full_path = uri + os.sep + dir_list[_]
                     # print(full_path)
                     self.dir_buttons.append(Button(text=dir_list[_], size_hint=(1, None), height="40dp",
-                                                   split_str="True", text_size=(Window.width - 20, None),
+                                                   split_str="True", text_size=(Window.width - dp(20), None),
                                                    on_press=partial(self.act, full_path)))
                     self.ids.main_layout.add_widget(self.dir_buttons[_])
                 self.ids.text_input.text = uri
@@ -96,37 +97,9 @@ class MainWidget(ScrollView):
                 self.ids.converted_json.text = str(e)
                 pass
 
-    # pass
-    # def __init__(self, **kwargs):
-    #     super().__init__(**kwargs)
-    #     self.orientation = "vertical"
-    #     b1 = Button(text="A")
-    #     b2 = Button(text="B")
-    #     b3 = Button(text="C")
-    #
-    #     self.add_widget(b1)
-    #     self.add_widget(b2)
-    #     self.add_widget(b3)
-
-
-class MainWidget(Widget):
-    pass
-
 
 class JsonReceiptConverterApp(App):
     pass
-    # from jnius import autoclass
-    #
-    # # test for an intent passed to us
-    # PythonActivity = autoclass('org.renpy.android.PythonActivity')
-    # activity = PythonActivity.mActivity
-    # intent = activity.getIntent()
-    # intent_data = intent.getData()
-    # try:
-    #     file_uri = intent_data.toString()
-    #     WidgetsExample.json_receipt_to_text(file_uri)
-    # except AttributeError:
-    #     file_uri = None
 
 
 JsonReceiptConverterApp().run()
